ed2cd9191b3bc5fa1e207b845c8c0fa27bbeebb4
Kindnesss.eth <kindnesss@superadditive.io>
2024-02-11 15:43:27 -0600

    Cleanup

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import { mainnet } from 'viem/chains'
 import { createEnsPublicClient } from '@ensdomains/ensjs'
 import { getTextRecord } from '@ensdomains/ensjs/public'
 
-
 (async () => {
   // Create the client
   const client = createEnsPublicClient({
@@ -12,43 +11,34 @@ import { getTextRecord } from '@ensdomains/ensjs/public'
     transport: http(),
   })
 
-  // Use the client
-  const ethAddress = await client.getAddressRecord({ name: 'wbtc.tkn.eth' })
-
   const records = await client.getRecords({
-    name: 'wbtc.tkn.eth',
-    records: {
-      coins: ['ETH', 'SOL', 'OP', 'ARB', 'AVAXC', 'BNB', 'CRO', 'FTM', 'GNO', 'MATIC', 'GOERLI', 'SEPOLIA', 'NEAR', 'TRX', 'ZIL'],
-      texts: [
-        'name', // Project name
-        'description', // Project description
-        'avatar', // Logo image URL
-        'url', // Main website URL
-        'notice', // Token data change notification
-        'decimals', // Number of decimals token can be divided by
-        'com.twitter', // Twitter handle
-        'com.github', // GitHub organization name
-        'com.discord', // Discord server invite string
-        'version', // Metadata versioning identifier
-        // Upcoming datapoints placeholders
-        'tokenSupply', // Total number of tokens created
-        'circulatingSupply', // Total number of tokens in circulation
-        'chainID', // Chain id if the token represents an EVM network
-        'coinType', // Coin type if the coin represents a non EVM network
-        'forum', // Project's forum URL
-        'governance', // Onchain voting interface URL
-        'snapshot', // Off-chain voting interface URL
-        'abi', // ABI interface for the token contract
-        'git', // Source code URL
-        'governanceContract', // Governance contract address
-        'canonicalDexPool', // Primary DEX pool URL
-      ],
-      contentHash: true
-    },
-  })
-  const result = await getTextRecord(client, {
-    name: 'wbtc.tkn.eth',
-    key: 'com.twitter',
+    name: 'matic.tkn.eth',
+    coins: ['ETH', 'SOL', 'OP', 'ARB1', 'AVAXC', 'BNB', 'CRO', 'FTM', 'GNO', 'MATIC', 'NEAR', 'TRX', 'ZIL', 'BASE'],
+    texts: [
+      'name', // Project name
+      'description', // Project description
+      'avatar', // Logo image URL
+      'url', // Main website URL
+      'notice', // Token data change notification
+      'decimals', // Number of decimals token can be divided by
+      'com.twitter', // Twitter handle
+      'com.github', // GitHub organization name
+      'com.discord', // Discord server invite string
+      'version', // Metadata versioning identifier
+      // Upcoming datapoints placeholders
+      'tokenSupply', // Total number of tokens created
+      'circulatingSupply', // Total number of tokens in circulation
+      'chainID', // Chain id if the token represents an EVM network
+      'coinType', // Coin type if the coin represents a non EVM network
+      'forum', // Project's forum URL
+      'governance', // Onchain voting interface URL
+      'snapshot', // Off-chain voting interface URL
+      'abi', // ABI interface for the token contract
+      'git', // Source code URL
+      'governanceContract', // Governance contract address
+      'canonicalDexPool', // Primary DEX pool URL
+    ]
+    // contenthash: true
   })
 
   console.log(records)
